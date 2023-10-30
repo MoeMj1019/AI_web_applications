902da0e72ce942daa28ac5f0b38076d485a58245
Edogor <periklismaskanakis@gmail.com>
2023-10-30 18:51:54 +0100

    gitignore /Test

---
+++ Test/testWrite.py
@@ -0,0 +1,3 @@
+a = list(range(1, 10))
+print(a.pop(0))
+print(a)
--- testWrite.py
+++
@@ -1,7 +0,0 @@
-import json
-
-thisDict = { "key": "value"}
-
-with open("safedIndexDataBase.json", 'w') as json_file:
-    json.dump(thisDict, json_file)
-print("Index DataBase was safed successfully :-)")

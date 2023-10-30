76699a0a4097d89af7c81bda8ca9dddfacbaf31f
Edogor <periklismaskanakis@gmail.com>
2023-10-30 19:51:50 +0100

    egal

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def index():
 def process():
     if request.method == 'POST':
         word = request.form['word']
+        
         # You can now process the entered 'word' variable here as needed
         processed_word = word.upper()  # Example: Convert the word to uppercase
         return render_template('result.html', processed_word=processed_word)

1d2065d9c7e3df8213c678b0667f484bf1fb93bf
Martin <maljammal@uni-osnabrueck.de>
2023-11-01 18:55:21 +0100

    deleted a forgotten json file from the repo

--- webCrawling.py
+++ webCrawling.py
@@ -262,36 +262,36 @@ if __name__ == "__main__":
     constraints_for_url = [SameDomain(allow_subdomains=True),NotVisitedRecently(time_delta=1, time_unit="days")]
     constraints_for_response = [ValidStatusCode(), ValidContentType()]
 
-    # initialize the crawler 
-    my_crawler = Crawler(
-        "https://vm009.rz.uos.de/crawl/index.html",
-        url_constraints=[SameDomain(allow_subdomains=True),NotVisitedRecently(time_delta=1, time_unit="days")],
-        response_constraints=[ValidStatusCode(), ValidContentType()],
-    )
-
-    # start crawling
-    my_crawler.run(
-        search_method="dfs",
-        max_iterations=100,
-    )
-
-    # save the data
-    my_crawler.search_index.saveData()
-    my_crawler.url_index.safe_data()
-
-    # initialize the crawler 
-    # my_crawler_1 = Crawler(
+    # # initialize the crawler 
+    # my_crawler = Crawler(
     #     "https://vm009.rz.uos.de/crawl/index.html",
-    #     url_constraints=[NotVisitedRecently(time_delta=1, time_unit="days")],
-    #     response_constraints=constraints_for_response,
+    #     url_constraints=[SameDomain(allow_subdomains=True),NotVisitedRecently(time_delta=1, time_unit="days")],
+    #     response_constraints=[ValidStatusCode(), ValidContentType()],
     # )
 
     # # start crawling
-    # my_crawler_1.run(
-    #     search_method="bfs",
-    #     max_iterations=1000,
+    # my_crawler.run(
+    #     search_method="dfs",
+    #     max_iterations=100,
     # )
 
     # # save the data
-    # my_crawler_1.search_index.saveData()
-    # my_crawler_1.url_index.safe_data()
+    # my_crawler.search_index.saveData()
+    # my_crawler.url_index.safe_data()
+
+    # initialize the crawler 
+    my_crawler_1 = Crawler(
+        "https://vm009.rz.uos.de/crawl/index.html",
+        url_constraints=[NotVisitedRecently(time_delta=1, time_unit="days")],
+        response_constraints=constraints_for_response,
+    )
+
+    # start crawling
+    my_crawler_1.run(
+        search_method="bfs",
+        max_iterations=1000,
+    )
+
+    # save the data
+    my_crawler_1.search_index.saveData()
+    my_crawler_1.url_index.safe_data()
